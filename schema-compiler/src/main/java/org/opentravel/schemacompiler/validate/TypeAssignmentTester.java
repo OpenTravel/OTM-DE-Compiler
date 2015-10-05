f76c9727e46bd6d9688608e3c362578d83f9790b
Steve Livezey <Stephen.Livezey@travelport.com>
2015-10-05 16:47:58 -0500

    Added additional methods to the TypeAssignmentChecker class.

--- schema-compiler/src/main/java/org/opentravel/schemacompiler/validate/TypeAssignmentTester.java
+++ schema-compiler/src/main/java/org/opentravel/schemacompiler/validate/TypeAssignmentTester.java
@@ -24,11 +24,16 @@ import org.opentravel.schemacompiler.ioc.CompilerExtensionRegistry;
 import org.opentravel.schemacompiler.model.NamedEntity;
 import org.opentravel.schemacompiler.model.TLAttribute;
 import org.opentravel.schemacompiler.model.TLAttributeType;
+import org.opentravel.schemacompiler.model.TLFacetType;
 import org.opentravel.schemacompiler.model.TLModel;
 import org.opentravel.schemacompiler.model.TLProperty;
 import org.opentravel.schemacompiler.model.TLPropertyType;
+import org.opentravel.schemacompiler.model.TLSimple;
+import org.opentravel.schemacompiler.model.TLSimpleFacet;
+import org.opentravel.schemacompiler.model.TLValueWithAttributes;
 import org.opentravel.schemacompiler.validate.compile.TLAttributeCompileValidator;
 import org.opentravel.schemacompiler.validate.compile.TLPropertyCompileValidator;
+import org.opentravel.schemacompiler.validate.compile.TLValueWithAttributesCompileValidator;
 import org.opentravel.schemacompiler.validate.impl.TLModelValidationContext;
 import org.opentravel.schemacompiler.validate.impl.TLValidationBuilder;
 
@@ -41,17 +46,33 @@ import org.opentravel.schemacompiler.validate.impl.TLValidationBuilder;
  */
 public class TypeAssignmentTester {
 	
-	private static final String ATTRIBUTE_ERROR_PREFIX = "org.opentravel.schemacompiler.TLAttribute.type.";
-	private static final String ELEMENT_ERROR_PREFIX   = "org.opentravel.schemacompiler.TLProperty.type.";
+	private static final String ATTRIBUTE_ERROR_PREFIX    = "org.opentravel.schemacompiler.TLAttribute.type.";
+	private static final String ELEMENT_ERROR_PREFIX      = "org.opentravel.schemacompiler.TLProperty.type.";
+	private static final String SIMPLE_ERROR_PREFIX       = "org.opentravel.schemacompiler.TLSimple.parentType.";
+	private static final String SIMPLE_FACET_ERROR_PREFIX = "org.opentravel.schemacompiler.TLSimpleFacet.simpleType.";
+	private static final String VWA_ERROR_PREFIX          = "org.opentravel.schemacompiler.TLValueWithAttributes.parentType.";
 	
 	private static Collection<String> typeAssignmentErrorKeys = Arrays.asList( new String[] {
+			// Attribute error codes
 			ATTRIBUTE_ERROR_PREFIX + TLValidationBuilder.INVALID_NAMED_ENTITY_REFERENCE,
 			ATTRIBUTE_ERROR_PREFIX + TLAttributeCompileValidator.ERROR_NON_SIMPLE_CORE_AS_ATTRIBUTE,
 			ATTRIBUTE_ERROR_PREFIX + TLAttributeCompileValidator.ERROR_ILLEGAL_LIST_FACET_REFERENCE,
 			ATTRIBUTE_ERROR_PREFIX + TLAttributeCompileValidator.ERROR_ILLEGAL_VWA_ATTRIBUTE,
 			ATTRIBUTE_ERROR_PREFIX + TLAttributeCompileValidator.ERROR_ILLEGAL_OPEN_ENUM_ATTRIBUTE,
+			
+			// Element error codes
 			ELEMENT_ERROR_PREFIX + TLValidationBuilder.INVALID_NAMED_ENTITY_REFERENCE,
-			ELEMENT_ERROR_PREFIX + TLPropertyCompileValidator.ERROR_ILLEGAL_LIST_FACET_REFERENCE
+			ELEMENT_ERROR_PREFIX + TLPropertyCompileValidator.ERROR_ILLEGAL_LIST_FACET_REFERENCE,
+			
+			// Simple type error codes
+			SIMPLE_ERROR_PREFIX + TLValidationBuilder.INVALID_NAMED_ENTITY_REFERENCE,
+			
+			// Simple facet error codes
+			SIMPLE_FACET_ERROR_PREFIX + TLValidationBuilder.INVALID_NAMED_ENTITY_REFERENCE,
+			
+			// VWA error codes
+			VWA_ERROR_PREFIX + TLValidationBuilder.INVALID_NAMED_ENTITY_REFERENCE,
+			VWA_ERROR_PREFIX + TLValueWithAttributesCompileValidator.ERROR_ILLEGAL_EXTENSION_ATTRIBUTE,
 	});
 	
 	private TLModel model;
@@ -59,6 +80,9 @@ public class TypeAssignmentTester {
 	private ValidatorFactory validatorFactory;
 	private Validator<TLAttribute> attributeValidator;
 	private Validator<TLProperty> elementValidator;
+	private Validator<TLSimple> simpleValidator;
+	private Validator<TLSimpleFacet> simpleFacetValidator;
+	private Validator<TLValueWithAttributes> vwaValidator;
 	
 	/**
 	 * Constructor that establishes the model context for all validation checks.\
@@ -122,6 +146,84 @@ public class TypeAssignmentTester {
 	}
 	
 	/**
+	 * Returns true if the given candidate is a valid base type assignment for the simple type.
+	 * 
+	 * @param simpleType  the simple type to which the candidate base type may be assigned
+	 * @param candidateBaseType  the candidate base type assignment for the simple
+	 * @return boolean
+	 */
+	public boolean isValidAssignment(TLSimple simpleType, NamedEntity candidateBaseType) {
+		boolean result;
+		
+		if (candidateBaseType instanceof TLAttributeType) {
+			TLSimple testSimple = new TLSimple();
+			
+			testSimple.setName("test");
+			testSimple.setOwningLibrary(simpleType.getOwningLibrary());
+			testSimple.setParentType((TLAttributeType) candidateBaseType);
+			
+			initValidators();
+			result = isLegalAssignment( simpleValidator.validate( testSimple ) );
+			
+		} else {
+			result = false;
+		}
+		return result;
+	}
+	
+	/**
+	 * Returns true if the given candidate is a valid type assignment for the simple facet.
+	 * 
+	 * @param element  the simple facet to which the candidate type may be assigned
+	 * @param candidateType  the candidate type assignment for the simple facet
+	 * @return boolean
+	 */
+	public boolean isValidAssignment(TLSimpleFacet simpleFacet, NamedEntity candidateType) {
+		boolean result;
+		
+		if (candidateType instanceof TLAttributeType) {
+			TLSimpleFacet testFacet = new TLSimpleFacet();
+			
+			testFacet.setFacetType(TLFacetType.SIMPLE);
+			testFacet.setOwningEntity(simpleFacet.getOwningEntity());
+			testFacet.setSimpleType((TLAttributeType) candidateType);
+			
+			initValidators();
+			result = isLegalAssignment( simpleFacetValidator.validate( testFacet ) );
+			
+		} else {
+			result = false;
+		}
+		return result;
+	}
+	
+	/**
+	 * Returns true if the given candidate is a valid base type assignment for the VWA.
+	 * 
+	 * @param element  the VWA to which the candidate base type may be assigned
+	 * @param candidateBaseType  the candidate base type assignment for the VWA
+	 * @return boolean
+	 */
+	public boolean isValidAssignment(TLValueWithAttributes vwa, NamedEntity candidateBaseType) {
+		boolean result;
+		
+		if (candidateBaseType instanceof TLAttributeType) {
+			TLValueWithAttributes testVWA = new TLValueWithAttributes();
+			
+			testVWA.setName("test");
+			testVWA.setOwningLibrary(vwa.getOwningLibrary());
+			testVWA.setParentType((TLAttributeType) candidateBaseType);
+			
+			initValidators();
+			result = isLegalAssignment( vwaValidator.validate( testVWA ) );
+			
+		} else {
+			result = false;
+		}
+		return result;
+	}
+	
+	/**
 	 * Returns true if none of the findings provided are flagged as illegal type assignments
 	 * for elements or attributes.
 	 * 
@@ -156,6 +258,9 @@ public class TypeAssignmentTester {
 			this.validatorFactory = factory;
 			attributeValidator = factory.getValidatorForClass( TLAttribute.class );
 			elementValidator = factory.getValidatorForClass( TLProperty.class );
+			simpleValidator = factory.getValidatorForClass( TLSimple.class );
+			simpleFacetValidator = factory.getValidatorForClass( TLSimpleFacet.class );
+			vwaValidator = factory.getValidatorForClass( TLValueWithAttributes.class );
 		}
 	}
 	
