78cb00ca4368bea8b2b973daa9c6709d67a4f771
stephen.livezey <Stephen.Livezey@travelport.com>
2018-07-26 08:22:03 -0600

    Fixed problem that caused filename conflicts for same-name resources in generated swagger files.

--- schema-compiler/src/main/java/org/opentravel/schemacompiler/codegen/impl/ResourceFilenameBuilder.java
+++ schema-compiler/src/main/java/org/opentravel/schemacompiler/codegen/impl/ResourceFilenameBuilder.java
@@ -15,7 +15,18 @@
  */
 package org.opentravel.schemacompiler.codegen.impl;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import org.opentravel.schemacompiler.codegen.CodeGenerationFilenameBuilder;
+import org.opentravel.schemacompiler.model.TLLibrary;
+import org.opentravel.schemacompiler.model.TLModel;
 import org.opentravel.schemacompiler.model.TLResource;
 
 /**
@@ -23,18 +34,133 @@ import org.opentravel.schemacompiler.model.TLResource;
  */
 public class ResourceFilenameBuilder implements CodeGenerationFilenameBuilder<TLResource> {
 	
+	private Map<TLResource,String> baseFilenameMap;
+	
 	/**
 	 * @see org.opentravel.schemacompiler.codegen.CodeGenerationFilenameBuilder#buildFilename(java.lang.Object, java.lang.String)
 	 */
 	@Override
 	public String buildFilename(TLResource resource, String fileExtension) {
+    	synchronized (this) {
+    		if (baseFilenameMap == null) {
+    			TLModel model = (resource == null) ? null : resource.getOwningModel();
+    			
+    			if (model != null) {
+        			initBaseFilenames( model );
+    			}
+    		}
+    	}
         String fileExt = (fileExtension.length() == 0) ? "" : ("." + fileExtension);
-        String filename = resource.getName() + "_" + resource.getVersion().replaceAll("\\.", "_");
-        
+    	String filename = baseFilenameMap.get( resource );
+    	
+    	if (filename == null) {
+            filename = new FilenameDetails( resource ).getFilename();
+    	}
         if (!filename.toLowerCase().endsWith(fileExt)) {
             filename += fileExt;
         }
         return filename;
 	}
 	
+    /**
+     * Initializes the filenames that should be used for each resource in the model.
+     * 
+     * @param model  the model that contains all resources to which names should be assigned
+     */
+	private void initBaseFilenames(TLModel model) {
+    	Set<FilenameDetails> filenameDetails = new HashSet<>();
+    	boolean conflictsExist;
+    	
+    	// Build the initial list of filename details
+    	for (TLLibrary library : model.getUserDefinedLibraries()) {
+    		for (TLResource resource : library.getResourceTypes()) {
+        		filenameDetails.add( new FilenameDetails( resource ) );
+    		}
+    	}
+    	
+    	// Check for conflicts and continue attempting to resolve until no more
+    	// conflicts exist, or no further options are available.
+    	do {
+    		Map<String,List<FilenameDetails>> detailsByFilename = new HashMap<>();
+    		
+    		for (FilenameDetails fd : filenameDetails) {
+    			List<FilenameDetails> detailsList = detailsByFilename.get( fd.getFilename() );
+    			
+    			if (detailsList == null) {
+    				detailsList = new ArrayList<>();
+    				detailsByFilename.put( fd.getFilename(), detailsList );
+    			}
+    			detailsList.add( fd );
+    		}
+    		conflictsExist = false;
+    		
+    		for (List<FilenameDetails> detailsList : detailsByFilename.values()) {
+    			if (detailsList.size() > 1) {
+    				boolean changesMade = false;
+    				
+    				for (FilenameDetails fd : detailsList) {
+    					if (!fd.nsComponents.isEmpty()) {
+        					fd.resourceFilename = fd.resource.getName() + "_" + fd.nsComponents.remove( 0 );
+    						changesMade = true;
+    					}
+    				}
+    				
+    				// If no more namespace options are available, allow the conflict to exist.  In this
+    				// situation, there are other errors in the model that should not have allowed us to
+    				// get this far.  Exiting at this point will prevent us from getting stuck in an
+    				// infinite loop.
+    				conflictsExist |= changesMade;
+    			}
+    		}
+    		
+    	} while (conflictsExist);
+    	
+    	baseFilenameMap = new HashMap<>();
+    	
+    	for (FilenameDetails fd : filenameDetails) {
+    		baseFilenameMap.put( fd.resource, fd.getFilename() );
+    	}
+    }
+    
+    /**
+     * Encapsulates the various details of a resource's filename (used during initialization).
+     */
+    private static class FilenameDetails {
+    	
+    	public TLResource resource;
+    	public List<String> nsComponents = new ArrayList<>();
+    	public String resourceFilename;
+    	public String versionSuffix;
+    	
+    	/**
+    	 * Constructor that assigns the initial values for each component of the filename
+    	 * details.
+    	 * 
+    	 * @param resource  the resource to which a filename will be assigned
+    	 */
+    	public FilenameDetails(TLResource resource) {
+    		String baseNS = resource.getBaseNamespace();
+    		
+    		this.resource = resource;
+    		this.resourceFilename = resource.getName();
+            this.versionSuffix = "_" + resource.getVersion().replaceAll("\\.", "_");
+    		
+            if (baseNS.endsWith("/")) {
+            	baseNS = baseNS.substring( 0, baseNS.length() - 1 );
+            }
+            this.nsComponents = new ArrayList<>( Arrays.asList( baseNS.split( "/" ) ) );
+            Collections.reverse( this.nsComponents );
+    	}
+    	
+    	/**
+    	 * Returns the filename as currently specified by these details.
+    	 * 
+    	 * @return String
+    	 */
+    	public String getFilename() {
+    		return resourceFilename + versionSuffix;
+    	}
+    	
+    }
+    
 }
